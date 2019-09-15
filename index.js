38f74f0a94791c3948f83bd485981e18eaebcb72
KateTor <48391659+KateTor@users.noreply.github.com>
2019-09-15 16:44:37 -0400

    setup server

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const moment = require("moment");
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(8081, "0.0.0.0", () => {
+    console.log("Server is running.");
+});
+
