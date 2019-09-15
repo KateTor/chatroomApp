2244671414435bfa4b73228793cf43ea9e51543d
KateTor <48391659+KateTor@users.noreply.github.com>
2019-09-15 18:41:49 -0400

    server set up, beginning jQueries to send data

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ app.use(bodyParser.json());
 
 const moment = require("moment");
 
+let messages = [];
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
+});
+
+app.put("/send", (req, res) => {
+    messages.push({
+        name: req.body.name,
+        message: req.body.message,
+        timestamp: new Date()
+    });
+
+    res.send({status: "ok"});
+});
+
+app.get("/receive", (req, res) => {
+    res.send(messages);
 });
 
 app.listen(8081, "0.0.0.0", () => {
